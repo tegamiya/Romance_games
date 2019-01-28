0d0a0f255dff6066e32da607794a5a3ca3ee2fbe
tegamiya <40752235+tegamiya@users.noreply.github.com>
2019-01-28 14:59:57 +0900

    Create mygame.h

---
+++ mygame.h
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+
+#ifndef MYGAME_H_
+#define MYGAME_H_
+
+#define MAX_M 4  //ミサイルの最大数
+#define PI 3.14159 //円周率
+
+#define MOJI_SIZE 16 //文字の大きさ
+#define SCRIPT_FILE 10000 //スクリプトファイルの大きさ
+
+
+#endif
